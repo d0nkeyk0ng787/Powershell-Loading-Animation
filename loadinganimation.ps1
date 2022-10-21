a2463643935b24ecf3290c4dc7968dcb63d75bce
Neb <77905135+d0nkeyk0ng787@users.noreply.github.com>
2022-10-21 21:26:55 +1100

    Create loadinganimation.ps1

---
+++ loadinganimation.ps1
@@ -0,0 +1,23 @@
+# Waiting animation script
+# Written by Gnome787
+
+$chars = "/", "-", "\", "|"
+$state = 1
+$counter = 0
+$mult = 1
+
+while($state -eq 1){
+    for ($i=0;$i -le 5; $i++) {
+        if($i -eq 4){
+            $i = 0
+        }
+        if($script:counter -eq 20){
+            $script:counter = 0
+            $script:mult++
+        }
+        $progress = $chars[$i] * $mult
+        Write-Host -NoNewLine "`r$progress"
+        $script:counter++
+        Start-Sleep -Milliseconds 150
+    }
+}
